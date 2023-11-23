0566996ce0592cc752f893ff252a4257fd6156d2
Ilyass Ouhsseine <101465486+samurai0lava@users.noreply.github.com>
2023-11-23 17:37:17 +0000

    Add

--- libft.h
+++ libft.h
@@ -13,12 +13,17 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <stdio.h>
-# include <string.h>
+// # include <stdio.h>
+// # include <string.h>
 # include <unistd.h>
 # include <stdlib.h>
-# include <limits.h>
 # include <stdint.h>
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
 
 void				*ft_memset(void *str, int c, size_t n);
 void				ft_bzero(void *str, size_t n);
@@ -57,12 +62,6 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
-
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}				t_list;
 t_list				*ft_lstnew(void *content);
 void				ft_lstadd_front(t_list **lst, t_list *new);
 int					ft_lstsize(t_list *lst);
