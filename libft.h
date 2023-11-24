4918169168602d9f4929f50ee933d9cbaa2c4467
Ilyass Ouhsseine <101465486+samurai0lava@users.noreply.github.com>
2023-11-24 19:14:07 +0000

    Add

--- libft.h
+++ libft.h
@@ -51,7 +51,6 @@ int					ft_toupper(int c);
 int					ft_tolower(int c);
 void				*ft_calloc(size_t nmemb, size_t size);
 char				*ft_strdup(const char *str);
-
 char				*ft_substr(char const *s, unsigned int start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
