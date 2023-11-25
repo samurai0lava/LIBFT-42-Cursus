3ca6f5a38843c105171a2a6f5f6e11ff98463e29
Ilyass Ouhsseine <101465486+samurai0lava@users.noreply.github.com>
2023-11-25 09:59:37 +0000

    Add

---
+++ ft_lstmap_bonus.c
@@ -0,0 +1,31 @@
+
+#include "libft.h"
+
+t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+    t_list *new_list = NULL;
+    t_list *current_node = NULL;
+
+    if(f == NULL || del == NULL)
+        return(NULL);
+    while (lst)
+    {
+        void    *new_content;
+        t_list  *new_node;
+        
+        new_content = f(lst->content);
+        new_node = ft_lstnew(new_content);
+        if (!new_node)
+        {
+            ft_lstclear(&new_list, del);
+            return (NULL);
+        }
+        if (!new_list)
+            new_list = new_node;
+        else
+            current_node->next = new_node;
+        current_node = new_node;
+        lst = lst->next;
+    }
+    return (new_list);
+}
